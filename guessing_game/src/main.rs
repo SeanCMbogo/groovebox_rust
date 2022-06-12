a8802bc6810554d1da70c4b00d51a57ef2271fb1
Sean Mbogo <sean.mbogo2@cox.com>
2022-06-11 21:01:20 -0400

    Guessing game is complete

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,26 +6,31 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
-
-    println!("Input your guess.");
-
-    // let apples = 5; // immutable
-    let mut guess = String::new(); // mutable
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {}", guess);
     loop {
+        println!("Input your guess.");
+
+        // let apples = 5; // immutable
+        let mut guess = String::new(); // mutable
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
-
-    println!("The secret number is: {}", secret_number);
+    // println!("The secret number is: {}", secret_number);
 }
